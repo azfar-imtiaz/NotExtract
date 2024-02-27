09c9c8e0ce1f6cc7db57d317d09f1e9f75f038a1
azfar.imtiaz <azfar.imtiaz@live.com>
2024-02-27 19:44:41 +0100

    Initial Commit

---
+++ Notextract/NotextractApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotextractApp.swift
+//  Notextract
+//
+//  Created by Azfar Imtiaz on 2024-02-27.
+//
+
+import SwiftUI
+
+@main
+struct NotextractApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
