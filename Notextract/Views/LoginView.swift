99f62187dcbd1497d13ee69ab86b03e7a38d759d
azfar.imtiaz <azfar.imtiaz@live.com>
2024-03-06 22:43:43 +0100

    Restructure login view

--- Notextract/Views/LoginView.swift
+++ Notextract/Views/LoginView.swift
@@ -38,152 +38,13 @@ struct LoginView: View {
                         .offset(y: logoOffsetY)
                         .opacity(isFieldInFocus ? 0.0 : 1.0)
                         .animation(Animation.easeInOut)
-                    
-                    // Spacer()
                 }
                 
-                VStack {
-                    if !isSignUpMode {
-                        Text("Welcome!")
-                            .font(.customFont("LeagueSpartan-Bold", size: 35))
-                            .foregroundStyle(.gold)
-                            .padding(.bottom, 30)
-                        
-                        TextField("Username", text: $username)
-                            .underlineTextField()
-                            .padding()
-                            .foregroundStyle(.ivory)
-                            .font(.customFont("LeagueSpartan-Regular", size: 20))
-                            .padding(.bottom, 10)
-                            .focused($isFieldInFocus)
-                        
-                        SecureField("Password", text: $password)
-                            .frame(minHeight: secureFieldHeight)
-                            .underlineTextField()
-                            .padding()
-                            .foregroundStyle(.ivory)
-                            .font(.customFont("LeagueSpartan-Regular", size: 20))
-                            .padding(.bottom, 20)
-                            .focused($isFieldInFocus)
-                        
-                        Button {
-                            print("Login pressed!")
-                        } label: {
-                            Text("Login")
-                                .foregroundStyle(.charcoal)
-                                .frame(width: 150)
-                                .font(.customFont("LeagueSpartan-Regular", size: 20))
-                                .padding()
-                                .background(.gold)
-                                .cornerRadius(5.0)
-                        }
-                        
-                        HStack(spacing: .zero) {
-                            Text("Don't have an account?")
-                                .foregroundStyle(.ivory)
-                                .font(.customFont("Lora-Regular", size: 15))
-                                .padding(.trailing, 5)
-                            
-                            Button {
-                                print("Sign up pressed!")
-                                withAnimation {
-                                    isSignUpMode.toggle()
-                                    // loginSectionOffsetY = isSignUpMode ? -300 : 0
-                                    loginFieldsHeight = isSignUpMode ? UIScreen.main.bounds.height : 500
-                                }
-                            } label: {
-                                Text("Sign up")
-                                    .foregroundStyle(.gold)
-                                    .font(.customFont("Lora-Regular", size: 15))
-                            }
-                        }
-                        .padding(.top, 30)
-                        .padding(.bottom, 20)
-                        .opacity(isFieldInFocus ? 0.0 : 1.0)
-                        
-                    } else {
-                        Spacer()
-                        
-                        Text("Let's get you signed up!")
-                            .font(.customFont("LeagueSpartan-Bold", size: 35))
-                            .foregroundStyle(.gold)
-                            .padding(20)
-                            .multilineTextAlignment(.center)
-                            .opacity(isFieldInFocus ? 0.0 : 1.0)
-                        
-                        TextField("Email", text: $username)
-                            .underlineTextField()
-                            .padding()
-                            .foregroundStyle(.ivory)
-                            .font(.customFont("LeagueSpartan-Regular", size: 20))
-                            .padding(.bottom, 10)
-                            .focused($isFieldInFocus)
-                        
-                        SecureField("Password", text: $password)
-                            .frame(minHeight: secureFieldHeight)
-                            .underlineTextField()
-                            .padding()
-                            .foregroundStyle(.ivory)
-                            .font(.customFont("LeagueSpartan-Regular", size: 20))
-                            .padding(.bottom, 10)
-                            .focused($isFieldInFocus)
-                        
-                        SecureField("Repeat Password", text: $repeatedPassword)
-                            .frame(minHeight: secureFieldHeight)
-                            .underlineTextField()
-                            .padding()
-                            .foregroundStyle(.ivory)
-                            .font(.customFont("LeagueSpartan-Regular", size: 20))
-                            .padding(.bottom, 20)
-                            .focused($isFieldInFocus)
-                        
-                        Spacer()
-                        
-                        Button {
-                            print("Sign up successful!!")
-                        } label: {
-                            Text("Sign up")
-                                .foregroundStyle(.charcoal)
-                                .frame(width: 150)
-                                .font(.customFont("LeagueSpartan-Regular", size: 20))
-                                .padding()
-                                .background(.gold)
-                                .cornerRadius(5.0)
-                        }
-                        
-                        HStack(spacing: .zero) {
-                            Text("Go back to")
-                                .foregroundStyle(.ivory)
-                                .font(.customFont("Lora-Regular", size: 15))
-                                .padding(.trailing, 5)
-                            
-                            Button {
-                                print("Going back to login!")
-                                withAnimation {
-                                    isSignUpMode.toggle()
-                                    // loginSectionOffsetY = isSignUpMode ? -300 : 0
-                                    loginFieldsHeight = isSignUpMode ? UIScreen.main.bounds.height : 500
-                                }
-                            } label: {
-                                Text("login")
-                                    .foregroundStyle(.gold)
-                                    .font(.customFont("Lora-Regular", size: 15))
-                            }
-                        }
-                        .padding(.top, 30)
-                        .padding(.bottom, 20)
-                        .opacity(isFieldInFocus ? 0.0 : 1.0)
-                        
-                        Spacer()
-                    }
+                if !isSignUpMode {
+                    loginSection()                    
+                } else {
+                    signUpSection()
                 }
-                .padding()
-                .frame(height: loginFieldsHeight)
-                .background(.charcoal)
-                .roundedCorner(30, corners: isSignUpMode ? [] : [.topLeft, .topRight])
-                .offset(y: loginSectionOffsetY)
-                .animation(Animation.easeInOut)
-                .padding(.bottom)
             }
             .background(.ivory)
         }
@@ -206,6 +67,161 @@ struct LoginView: View {
                 }
             }
         }
+    }
+}
+
+extension LoginView {
+    func loginSection() -> some View {
+        VStack {
+            Text("Welcome!")
+                .font(.customFont("LeagueSpartan-Bold", size: 35))
+                .foregroundStyle(.gold)
+                .padding(.bottom, 30)
+            
+            TextField("Username", text: $username)
+                .underlineTextField()
+                .padding()
+                .foregroundStyle(.ivory)
+                .font(.customFont("LeagueSpartan-Regular", size: 20))
+                .padding(.bottom, 10)
+                .focused($isFieldInFocus)
+            
+            SecureField("Password", text: $password)
+                .frame(minHeight: secureFieldHeight)
+                .underlineTextField()
+                .padding()
+                .foregroundStyle(.ivory)
+                .font(.customFont("LeagueSpartan-Regular", size: 20))
+                .padding(.bottom, 20)
+                .focused($isFieldInFocus)
+            
+            Button {
+                print("Login pressed!")
+            } label: {
+                Text("Login")
+                    .foregroundStyle(.charcoal)
+                    .frame(width: 150)
+                    .font(.customFont("LeagueSpartan-Regular", size: 20))
+                    .padding()
+                    .background(.gold)
+                    .cornerRadius(5.0)
+            }
+            
+            HStack(spacing: .zero) {
+                Text("Don't have an account?")
+                    .foregroundStyle(.ivory)
+                    .font(.customFont("Lora-Regular", size: 15))
+                    .padding(.trailing, 5)
+                
+                Button {
+                    print("Sign up pressed!")
+                    withAnimation {
+                        isSignUpMode.toggle()
+                        // loginSectionOffsetY = isSignUpMode ? -300 : 0
+                        loginFieldsHeight = isSignUpMode ? UIScreen.main.bounds.height : 500
+                    }
+                } label: {
+                    Text("Sign up")
+                        .foregroundStyle(.gold)
+                        .font(.customFont("Lora-Regular", size: 15))
+                }
+            }
+            .padding(.top, 30)
+            .padding(.bottom, 20)
+            .opacity(isFieldInFocus ? 0.0 : 1.0)
+        }
+        .padding()
+        .frame(height: loginFieldsHeight)
+        .background(.charcoal)
+        .roundedCorner(30, corners: isSignUpMode ? [] : [.topLeft, .topRight])
+        .offset(y: loginSectionOffsetY)
+        .animation(Animation.easeInOut)
+        .padding(.bottom)
+    }
+    
+    func signUpSection() -> some View {
+        VStack {
+            Spacer()
+            
+            Text("Let's get you signed up!")
+                .font(.customFont("LeagueSpartan-Bold", size: 35))
+                .foregroundStyle(.gold)
+                .padding(20)
+                .multilineTextAlignment(.center)
+                .opacity(isFieldInFocus ? 0.0 : 1.0)
+            
+            TextField("Email", text: $username)
+                .underlineTextField()
+                .padding()
+                .foregroundStyle(.ivory)
+                .font(.customFont("LeagueSpartan-Regular", size: 20))
+                .padding(.bottom, 10)
+                .focused($isFieldInFocus)
+            
+            SecureField("Password", text: $password)
+                .frame(minHeight: secureFieldHeight)
+                .underlineTextField()
+                .padding()
+                .foregroundStyle(.ivory)
+                .font(.customFont("LeagueSpartan-Regular", size: 20))
+                .padding(.bottom, 10)
+                .focused($isFieldInFocus)
+            
+            SecureField("Repeat Password", text: $repeatedPassword)
+                .frame(minHeight: secureFieldHeight)
+                .underlineTextField()
+                .padding()
+                .foregroundStyle(.ivory)
+                .font(.customFont("LeagueSpartan-Regular", size: 20))
+                .padding(.bottom, 20)
+                .focused($isFieldInFocus)
+            
+            Spacer()
+            
+            Button {
+                print("Sign up successful!!")
+            } label: {
+                Text("Sign up")
+                    .foregroundStyle(.charcoal)
+                    .frame(width: 150)
+                    .font(.customFont("LeagueSpartan-Regular", size: 20))
+                    .padding()
+                    .background(.gold)
+                    .cornerRadius(5.0)
+            }
+            
+            HStack(spacing: .zero) {
+                Text("Go back to")
+                    .foregroundStyle(.ivory)
+                    .font(.customFont("Lora-Regular", size: 15))
+                    .padding(.trailing, 5)
+                
+                Button {
+                    print("Going back to login!")
+                    withAnimation {
+                        isSignUpMode.toggle()
+                        // loginSectionOffsetY = isSignUpMode ? -300 : 0
+                        loginFieldsHeight = isSignUpMode ? UIScreen.main.bounds.height : 500
+                    }
+                } label: {
+                    Text("login")
+                        .foregroundStyle(.gold)
+                        .font(.customFont("Lora-Regular", size: 15))
+                }
+            }
+            .padding(.top, 30)
+            .padding(.bottom, 20)
+            .opacity(isFieldInFocus ? 0.0 : 1.0)
+            
+            Spacer()
+        }
+        .padding()
+        .frame(height: loginFieldsHeight)
+        .background(.charcoal)
+        .roundedCorner(30, corners: isSignUpMode ? [] : [.topLeft, .topRight])
+        .offset(y: loginSectionOffsetY)
+        .animation(Animation.easeInOut)
+        .padding(.bottom)
     }
 }
 
